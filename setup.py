83dde0d636bac2fd9602449d80d02abfdca2718b
Steven Firth <s.k.firth@lboro.ac.uk>
2019-12-02 12:28:19 +0000

    updates

--- setup.py
+++ setup.py
@@ -2,15 +2,24 @@
 
 
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(  
     name='hoboreader', 
-    version='0.0.1',  
-    description='Python package for reading Onset Hobo sensor csv files',  # Required
-    url='https://github.com/building-energy/hoboreader',  # Optional
-    author='Steven Firth',  # Optional
+    version='v0.0.1',  
+    author='Steven K Firth',  # Optional
     author_email='s.k.firth@lboro.ac.uk',  # Optional
-    packages=find_packages() # Required
+    description='Python package for reading Onset Hobo sensor csv files',  # Required
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/building-energy/hoboreader',  # Optional
+    packages=find_packages(), # Required
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     )
